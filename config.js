75d6c961812bbab81c4133175fd5920119c22753
GamerSatoruYT <tommykoroma15@gmail.com>
2024-07-24 12:45:33 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['23279816937', 'Gamer Satoru YT', true],
+  ['23279816937', 'Gamer Satoru YT', true], 
   [''] 
 ] //Number of owners
 
